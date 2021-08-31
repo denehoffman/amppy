8387d54e50983fa3be09e7769a9972dc6fd8bce4
Nathaniel D. Hoffman <36977879+denehoffman@users.noreply.github.com>
2021-08-31 19:28:23 -0400

    fix default argument

--- src/amppy/dividers/Divider.py
+++ src/amppy/dividers/Divider.py
@@ -220,7 +220,7 @@ class Divider(ABC):
         pass
 
     
-    def divide(self, low, high, nbins=20, root, config, data, gen, acc, bkg=None, variable_name='mass', **kwargs):
+    def divide(self, low, high, nbins, root, config, data, gen, acc, bkg=None, variable_name='mass', **kwargs):
         self.preprocessing(**kwargs)
         self.n_bins = nbins 
         self.low_high_tuple = (low, high)
