d8b40bd81a50c45a7d5ab5fe5c7100dbe8d50f9f
denehoffman <denehoffman@me.com>
2021-08-31 12:23:49 -0400

    remove build and dist directories

--- build/lib.linux-x86_64-3.6/amppy/dividers/__init__.py
+++
@@ -1,4 +0,0 @@
-from __future__ import absolute_import
-
-from .Divider import Divider
-from .Divider_split_mass import Divider_split_mass
--- build/scripts-2.7/sbatch_job.csh
+++
@@ -1,2 +0,0 @@
-#!/bin/tcsh -f
-amppy_fit.py $1 $2 $3 $4 $5 $6 $7
